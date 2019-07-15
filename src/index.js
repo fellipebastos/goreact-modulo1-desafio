f68078032f7fba0cb029bb783f86bfbb9df80b0f
fellipedotlib <fellipe.bastos@dotlib.com.br>
2019-07-15 15:50:14 -0300

    prepares styles and make structures

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// React Import
+import React, {Component, Fragment } from 'react';
+import { render } from 'react-dom';
+
+// Styles Import
+import './assets/reset.css';
+import './assets/styles.scss';
+
+class App extends Component {
+  render() {
+    return '';
+  }
+}
+
+render(<App />, document.getElementById("app"));
